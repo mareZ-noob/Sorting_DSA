f33bfec1ab4564b72fc6a2de66c4656905d1f334
mareZ-noob <namnguyen160504@gmail.com>
2023-07-13 14:50:25 +0700

    second commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
 #include "main.h"
 
-int main(int argc, char** argv) { return 0; }
+int main(int argc, char** argv) { 
+    if (argc == 5) {
+ 
+    } else if (argc == 6) {
+        
+    }    
+    
+    return 0; 
+}
