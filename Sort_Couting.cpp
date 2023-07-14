30236f08818a31ba958cd72b965f1594512834ab
huymessi2706 <huymessi147@gmai.com>
2023-07-14 17:40:46 +0700

    count-radix

--- Sort_Couting.cpp
+++ Sort_Couting.cpp
@@ -96,20 +96,64 @@ long long QuickSortCounting(int a[], int n) {
 // O(n)
 long long CountingSortCounting(int a[], int n) {
     long long cnt = 0;
+    
+    int max_val = a[0];
+    for(int i = 1 ; ++cnt && i < n ; i++)
+        if(++cnt && max_val < a[i])
+            max_val = a[i];
 
-    /*
-        TODO
-    */
+    int *count = new int [max_val + 1];
+    for(int i = 0 ; ++cnt && i < n ; i++)
+        count[a[i]]++;
+
+    for(int i = 1 ; ++cnt && i <= max_val ; i++)
+        count[i] = count[i] + count[i-1];
+
+    int* b = new int [n];
+    for(int i = n - 1 ; ++cnt && i >= 0 ; i--){
+        b[count[a[i]] - 1]  = a[i];
+        count[a[i]] --;
+    }
+
+    for(int i = 0 ; ++cnt && i < n ; i++)
+        a[i] = b[i];
 
     return cnt;
 }
 
 long long RadixSortCounting(int a[], int n) {
     long long cnt = 0;
+    
+    int max_val = a[0];
+    for(int i = 1 ; ++cnt && i < n ; i++)
+        if(++cnt && a[i] > max_val)
+            max_val = a[i];
+    
+    int digits = 0, div;
+    do{
+        digits++;
+        div = max_val/pow(10,digits);
+    }while(div);
 
-    /*
-        TODO
-    */
+    int* tmp_arr[10];
+    for(int i = 0 ; ++cnt && i < 10 ; i++)
+        tmp_arr[i] = new int [n];
+
+    int tmp_count[10];
+
+    for(int i = 1 ; ++cnt && i < max_val ; i*= 10){
+        for(int j = 0 ; ++cnt && j < 10 ; j++)  
+            tmp_count[j] = 0;
+        for(int j = 0 ; ++cnt && j < n ; j++){
+            int idx = a[j]/i%10;
+            tmp_arr[idx][tmp_count[idx]++] = a[j];
+        }
+
+        int idx = 0;
+        for(int j = 0 ; ++cnt && j < 10 ; j++)
+            for(int k = 0 ; ++cnt && k < tmp_count[j] ; k++)
+                a[idx++] = tmp_arr[j][k];
+    }
 
     return cnt;
 }
