ab8abaea89ab369948a609ac11a9729844b16d61
Bi19tuoi <Lamchitaia1@gmail.com>
2023-07-14 18:50:29 +0700

    Add Sort_time.cpp

--- Sort_Time.cpp
+++ Sort_Time.cpp
@@ -1,1 +1,15 @@
 #include "Sort_Time.h"
+
+double SelectionSortTime(int a[], int n){
+    clock_t start = clock();
+    for(int i = 0; i < n - 1; i++){
+		int minId = i;
+		for(int j = i + 1; j < n; j++){
+			if(a[j] < a[minId])
+				minId = j;
+		}
+		swap(a[i], a[minId]);
+	}
+    clock_t stop = clock();
+    return double(stop - start) / CLOCKS_PER_SEC; 
+}
